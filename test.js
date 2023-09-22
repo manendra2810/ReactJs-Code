16c0768710dd6b0d77c19b747d03029fd0d59cee
Manendra Agnihotri <agnihotri2810@gmail.com>
2023-09-22 21:46:07 +0530

    Git code added

---
+++ test.js
@@ -0,0 +1,13 @@
+//named export 
+export function test1() {
+    console.log("h1");
+}
+
+export function test2() {
+    console.log("h2");
+}
+
+//default export 
+export default function DefaultTest(){
+    console.log("h3");
+}
